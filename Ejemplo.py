51e0fa0351ceb2e7f3420f72f53bc67ad1a9f3bd
Javier Cardenas (Americas) <javier.cardenas@dimensiondata.com>
2018-05-26 12:48:57 -0400

    commit

---
+++ Ejemplo.py
@@ -0,0 +1,8 @@
+import os, sys
+a = int(input("numero1 :"))
+b = int(input("numero2 :"))
+c = int(input("numero3 :"))
+d = int(input("numero4 :"))
+resultado = a+b+c+d
+cadena="la suma es"
+print(cadena, resultado)
---
+++ Suma.py
@@ -0,0 +1,6 @@
+#C:\Python27
+# _*_ coding: iso-8859-15 _*_
+import os, sys
+a = int(input("Unidades :"))
+b = int(input("Precio :"))
+print("A pagar=", a*b)
